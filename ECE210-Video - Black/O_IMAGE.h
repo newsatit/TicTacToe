08502dee061d08df6c214649647622d4c497bdd4
Dawanit Satitsumpum <dawanit@Dawanits-MacBook-Pro.local>
2018-04-25 19:19:31 -0500

    Add image files

---
+++ ECE210-Video - Black/O_IMAGE.h
@@ -0,0 +1,10 @@
+#ifndef __O_H__
+#define __O_H__
+
+#include "ece210_api.h"
+
+extern const uint8_t invader_bitmap[];
+#define O_WIDTH_PXL 50
+#define O_HEIGHT_PXL 50
+
+#endif
---
+++ ECE210-Video - Black/x_IMAGE_BITMAP.h
@@ -0,0 +1,10 @@
+#ifndef __X_H__
+#define __X_H__
+
+#include "ece210_api.h"
+
+extern const uint8_t X_bitmap[];
+#define X_WIDTH_PXL 50
+#define X_HEIGHT_PXL 50
+
+#endif
