c619de65ef0997ba925d8908f1f45fcf448261f3
zzhou292 <zzhou292@wisc.edu>
2018-04-25 19:04:23 -0500

    Full code

--- ECE210-Video - Black/main.c
+++ ECE210-Video - Black/main.c
@@ -23,7 +23,8 @@ uint32_t character;              //if character==0, then I'm "X"
                                  //if character==1, then I'm "O"
 //**********FUNCTION DECLARATION*********************
 void start();
-
+void play();
+void select();
 //***************************************************
 int main(void)
 {
@@ -39,7 +40,7 @@ void start()
     //**************************************************
     //display message first
     
-    ece210_lcd_add_msg("Welcome to TIC-TAC-TOE", TERMINAL_ALIGN_CENTER, LCD_COLOR_RED);             
+   ece210_lcd_add_msg("Welcome to TIC-TAC-TOE", TERMINAL_ALIGN_CENTER, LCD_COLOR_RED);             
     ece210_lcd_add_msg("Press Any Key to Start", TERMINAL_ALIGN_CENTER, LCD_COLOR_BLUE);
     ece210_lcd_add_msg("Jason Zhou; Dawanit Satitsumpun; Avinash Narisetty", TERMINAL_ALIGN_CENTER, LCD_COLOR_YELLOW);
     
@@ -56,7 +57,7 @@ void start()
         if(ece210_wireless_data_avaiable()) {
             ready = ece210_wireless_get();
             if(ready) {
-               
+                select();
             }
         }
         
@@ -65,9 +66,94 @@ void start()
             AlertButtons=false; //set AlertButton to false
             ready = 1;
             ece210_wireless_send(1);
-            
+            select(); //leads to the charater selection page
         }
     }
 }
 
+void select()
+{
+    //********************LOCAL VARIABLE*******************
+    uint8_t info[2]={PIC_X,PIC_O};
+    unit32_t curIndex=0;
+    unint32_t otherPlayer;
+    unint8_t direction;
+    //*****************************************************
+    //display the message first
+    ece210_lcd_add_msg("Press Right Button to select between X and O", TERMINAL_ALIGN_CENTER, LCD_COLOR_GREEN)
+    ece210_lcd_add_msg("X Goes First", TERMINAL_ALIGN_CENTER, LCD_COLOR_RED);
+    
+    ece210_lcd_draw_rectangle(40,100,160, 5);       
+    ece210_lcd_draw_rectangle(40,220,160, 5);
+    ece210_lcd_draw_rectangle(40,100, 5, 120);
+    ece210_lcd_draw_rectangle(120,100, 5, 120);
+    ece210_lcd_draw_rectangle(200,100, 5, 120);
+    
+    ece210_lcd_draw_rectangle();        //draw the left rectangle in black
+    ece210_lcd_draw_rectangle();         //draw the right rectangle in black
+    
+    ece210_lcd_draw_image();            //draw the "x" on the left black rectangle
+    ece210_lcd_draw_image();            //draw the "o" on the right black rectangle
+    
+    while(1)
+    {
+        otherPlayer = 0;
+        if(ece_wireless_data_available()) {
+            otherPlayer = ece210_wireless_get();
+            // opponent chose X
+            if(otherPlayer == 0) {
+                character = 1;
+            // opponent chose O    
+            } else if(otherPlayer == 1) {
+                character = 0;
+            }
+            select();
+            
+            break;
+        }
+        
+        ece210_lcd_draw_rectangle();    //clear the pic from the screen
+        delay(500);
+        ece210_lcd_draw_image();    //draw the index 0 pic to the screen
+        delay(500);                 //make it flashing
+        
+        direction=ece210_ps2_read_position();   
+        switch(direction)
+        {
+            case PS2_RIGHT:
+            {
+                curIndex=1;
+                break;
+            }
+            
+            case PS2_LEFT:
+            {
+                curIndex=0;
+                break;
+            }
+            
+            default:
+            {
+                break;
+            }
+        }
+        
+        if(AlertButtons)
+        {
+            AlertButtons=false;
+            if(btn_down_pressed())
+            {
+                ece210_wireless_send(curIndex);
+                character = curIndex;
+                play();
+                break;
+            }
+        }
+    }
+}
+
+void play()        
+{
+    
+}
 
